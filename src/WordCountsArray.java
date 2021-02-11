4d9065c186c9dc7f2592ab49f0907d8f51a9abbe
Thomas Kristan <tk.thomas.kristan@hotmail.com>
2021-02-11 17:27:06 +0100

    Improving WordCountsArray class

--- src/WordCountsArray.java
+++ src/WordCountsArray.java
@@ -1,26 +1,112 @@
-public class WordCountsArray {
-    //Attributes
-    private WordCount[] wordArr;
+/**
+ * This class represents a set of words and their counts.
+ * This class ensures that no empty words are added
+ * and that the word count is always greater than or equal to 0.
+ */
 
-    //Constructor
-    public WordCountsArray(int initSize){
-        if (initSize < 0) {
-            this.wordArr = new WordCount[0];
+public class WordCountsArray {
+    //<editor-fold desc="Attributes">
+    /**
+     * the administered WordCount objects
+     */
+    private WordCount[] wordArr;
+    /**
+     * the actual number of administered WordCount objects
+     */
+    private int actualSize;
+    /**
+     * the maximum number of administrable WordCount objects (not final)
+     */
+    private int maxSize;
+    //</editor-fold>
+
+    /**
+     * Creates a new instance of this class
+     * The created instance is able of administering at most maxSize words.
+     * @param maxSize   maximum number of words that can be administered by this instance
+     */
+    public WordCountsArray(int maxSize){
+        if (maxSize < 0) {
+            this.maxSize = 0;
         } else {
-            this.wordArr = new WordCount[initSize];
+            this.maxSize = maxSize;
+        }
+        this.actualSize = 0;
+        this.wordArr = new WordCount[this.maxSize];
+    }
+
+    /**
+     * Adds the specified word with the specified count to this instance.
+     * If the specified word is already administered by this instance, then the
+     * count of the specified word is increased by the given count.
+     * If the specified word is not administered already by this instance, this
+     * method creates a new wordCount instance and administeres it with its count.
+     * If the specified word is null or empty nothing happens.
+     * If the count is lower than 0, nothing will happen.
+     * @param word      the word to be added
+     * @param count     the count of the word to be added
+     */
+    public void add (String word, int count){
+        if(word == null || word == ""){
+            return;
+        }
+        if(count < 0){
+            return;
+        }
+        //if word is already administered, get index of the word
+        int index = getIndexOfWord(word.toLowerCase());
+        //word has not been found
+        if(index == -1){
+            //check if end of array has been reached -> increase array size
+            if(actualSize == maxSize){
+                this.doubleSize();
+            }
+            this.wordArr[actualSize] = new WordCount(word.toLowerCase(), count);
+            this.actualSize++;
+        //word has been found
+        } else {
+            this.wordArr[index].incrementCount(count);
         }
     }
 
-    public void add (String word, int count){
-        if((word != null) && word.length() != 0){
-            int firstEmptyIndex = this.size();
-            if(firstEmptyIndex != wordArr.length) {
-                wordArr[firstEmptyIndex] =new WordCount(word, count);
-            }
-            WordCount[] tempArr = new WordCount[wordArr.length + 1];
-            wordArr = this.copyContent(tempArr);
-            wordArr[wordArr.length -1] = new WordCount(word, count);
+    /**
+     * Doubles the number of administrable wordCount objects
+     */
+    private void doubleSize(){
+        this.maxSize = this.maxSize * 2;
+        if(this.maxSize <= 0){
+            this.maxSize = 1;
         }
+        WordCount[] newWordArr = new WordCount[this.maxSize];
+        //copy content of old array
+        for(int i=0; i < this.wordArr.length; i++){
+            newWordArr[i] = this.wordArr[i];
+        }
+        this.wordArr = newWordArr;
+    }
+
+    /**
+     * Determines whether the words administered by this instance and the words in the specified
+     * wordCountsArray are equal.
+     * @param wca   the wordCountsArray instance that will be compared.
+     * @return      true, if the words administered by this instance and the words administered
+     * in the specified wordCountsArray instance are the same AND the words are in the same
+     * order. Otherwise false.
+     */
+    private boolean wordsEqual(WordCountsArray wca){
+        //exactly the same
+        if(this == wca)
+            return true;
+        //cannot be the same
+        if((wca == null) || (this.size() != wca.size())){
+            return false;
+        }
+        for(int i=0; i<this.size(); i++){
+            if(!this.getWord(i).equals(wca.getWord(i))){
+                return false;
+            }
+        }
+        return true;
     }
 
     public int size(){
@@ -81,6 +167,17 @@ public class WordCountsArray {
         }
         return true;
     }
-
+ 
+    public int getIndexOfWord(String word){
+        if (word == null || word.equals("")){
+            return -1;
+        }
+        for(int i=0; i<this.size(); i++){
+            if(this.wordArr[i].getWord().equals(word)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
